6da998718f4e3ea325aca35a3d9a94d9f89a0f82
duongcuong4395 <dangduongcuong@gmail.com>
2025-12-24 10:15:28 +0700

    init

---
+++ FootballDt/FootballDt/FootballDtApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballDtApp.swift
+//  FootballDt
+//
+//  Created by Macbook on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballDtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
